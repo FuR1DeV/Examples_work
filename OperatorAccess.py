d108f826f58e7be2ac43dea5cf5044e238afad7f
Vasilii Turtugeshev <70282280+FuR1DeV@users.noreply.github.com>
2024-01-18 00:06:06 +0300

    Update OperatorAccess.py

--- OperatorAccess.py
+++ OperatorAccess.py
@@ -10,10 +10,6 @@ class OperatorAccess(AccessMiddleware):
         if dto.email:
             if await model.exists(username=dto.email, deleted=False):
                 raise InconsistencyError(message=f"{model.__name__} already exists")
-        crypted_password, salt = BaseCrypto.encrypt_password(dto.password)
-        dto.password = crypted_password
-        dto.__dict__['salt'] = salt
-
         if not dto.email and not dto.username:
             raise InconsistencyError(message="Need email or username!")
         if not dto.password and not dto.pin:
